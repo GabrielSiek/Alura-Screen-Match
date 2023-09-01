1521bd83278d82a2c7f9f15d5ea4939c6fb80ffd
Gabriel Siek <130788757+GabrielSiek@users.noreply.github.com>
2023-09-01 15:47:07 -0300

    Update MainAPI.java

--- src/main/MainAPI.java
+++ src/main/MainAPI.java
@@ -6,38 +6,60 @@ import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class MainAPI {
 
     public static void main(String[] args) throws IOException, InterruptedException {
         Scanner sc = new Scanner(System.in);
-
-        System.out.print("Digite o filme: ");
-        String filme = sc.nextLine();
-        filme = filme.replace(' ', '+');
-
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://www.omdbapi.com/?t=" + filme +"&apikey=8a09ee7d"))
-                .build();
-        HttpResponse<String> response = client
-                .send(request, HttpResponse.BodyHandlers.ofString());
-
-        //System.out.println(response.body());
-
-        String json = response.body();
-
+        String filme = "";
+        List<Titulo> titulos = new ArrayList<>();
         Gson gson = new GsonBuilder().
-                setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE).create();
-        TituloOmdb tituloOmdb = gson.fromJson(json, TituloOmdb.class);
+                setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
+                .setPrettyPrinting()
+                .create();
 
-        Titulo titulo = new Titulo(tituloOmdb);
-        System.out.println(titulo);
+        while (!filme.equalsIgnoreCase("sair")) {
+            System.out.print("Digite o filme: ");
+            filme = sc.nextLine();
+            filme = filme.replace(' ', '+');
+
+            if(filme.equalsIgnoreCase("sair"))
+                break;
+
+            try {
+                HttpClient client = HttpClient.newHttpClient();
+                HttpRequest request = HttpRequest.newBuilder()
+                        .uri(URI.create("https://www.omdbapi.com/?t=" + filme + "&apikey=8a09ee7d"))
+                        .build();
+                HttpResponse<String> response = client
+                        .send(request, HttpResponse.BodyHandlers.ofString());
+
+                String json = response.body();
+
+                TituloOmdb tituloOmdb = gson.fromJson(json, TituloOmdb.class);
+
+                Titulo titulo = new Titulo(tituloOmdb);
+
+                titulos.add(titulo);
+            } catch (Exception e){
+                System.out.println(e);
+            }
+        }
+        System.out.println(titulos);
+
+        FileWriter escrita = new FileWriter("filmes.json");
+        escrita.write(gson.toJson(titulos));
+        escrita.close();
+
+        System.out.println("programa finalizado");
     }
 }
